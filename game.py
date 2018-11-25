17ae9ab9dce62c7ad8d2ffc053a737e322c8a665
kenttoshima <24969845+kenttoshima@users.noreply.github.com>
2018-11-25 18:28:15 -0500

    implement game without rotating the shape

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import randint
 
 class Grid(object):
     def __init__(self, width, height):
@@ -35,48 +35,50 @@ class Grid(object):
     # add given shape to given coordinate on down_left point
     def add_shape(self, shape, x, y):
         idx_r, idx_c = self.cord(x, y)
-        if y - 1 + shape.height > self.height or x - 1 + shape.width > self.width:
+        if y - 1 + shape.shapeHeight > self.height or x - 1 + shape.shapeWidth > self.width:
             raise ValueError
         for off_y, row in enumerate(shape.aslist()):
             for off_x, cell in enumerate(row):
                 self.grid[off_y + idx_r - len(shape.aslist()) + 1][off_x + idx_c] += cell
     
     # remove row of given y if the row is filled
-    def remove_row(self, y):
-        row = self.grid[self.cord(y = y)[0]]
+    def remove_row(self, c):
+        row = self.grid[c]
         if 0 not in row:
-            del row
+            del self.grid[c]
             self.grid.insert(0, [0 for i in range(self.width)])
         else:
             raise ValueError("Row not filled")
 
-class Shape:
+class Shape(object):
     def __init__(self, type):
         self.type = type
         self.shape_types = [
-        [[1, 1, 1],
-        [0, 1, 0]],
-        
-        [[0, 2, 2],
-        [2, 2, 0]],
-        
-        [[3, 3, 0],
-        [0, 3, 3]],
-        
-        [[4, 0, 0],
-        [4, 4, 4]],
-        
-        [[0, 0, 5],
-        [5, 5, 5]],
-        
-        [[6, 6, 6, 6]],
-        
-        [[7, 7],
-        [7, 7]]
-        ]
+            [[0]],
+            
+            [[1, 1, 1],
+            [0, 1, 0]],
+            
+            [[0, 2, 2],
+            [2, 2, 0]],
+            
+            [[3, 3, 0],
+            [0, 3, 3]],
+            
+            [[4, 0, 0],
+            [4, 4, 4]],
+            
+            [[0, 0, 5],
+            [5, 5, 5]],
+            
+            [[6, 6, 6, 6]],
+            
+            [[7, 7],
+            [7, 7]]
+            ]
         self.shape = self.shape_types[type]
-        self.height = len(self.shape)
-        self.width = len(self.shape[0])
+        self.shapeHeight = len(self.shape)
+        self.shapeWidth = len(self.shape[0])
     
     def __str__(self):
         string = ""
@@ -93,17 +95,18 @@ class Shape:
         self.shape = [ [ self.shape[y][x]
 			            for y in range(len(self.shape)) ]
 		                for x in range(len(self.shape[0]) - 1, -1, -1) ]
-        self.height = len(self.shape)
-        self.width = len(self.shape[0])
+        self.shapeHeight = len(self.shape)
+        self.shapeWidth = len(self.shape[0])
 
 class Shapes(Shape):
     def __init__(self, infinite = True, type_list = []):
+        super(Shapes, self).__init__(0)
         self.infinite = infinite
-        self.shape_list = map(lambda x: super(Shapes, self).__init__(type), type_list)
-    
+        self.shape_list = [self.shape] + map(lambda type: Shape(type), type_list)
+        
     def generate(self, turn):
         if self.infinite:
-            self.shape_list.append(choice(self.shape_types))
+            self.shape_list.append(Shape(randint(1, len(self.shape_types) - 1)))
         if turn > len(self.shape_list):
             raise ValueError
         else:
@@ -130,23 +133,42 @@ class Configuration(Grid):
             self.add_shape(shape, x, max(height_list) + 1)
         except ValueError as g:
             print g
+    
+    def clear(self):
+        cleared = 0
+        for i in range(len(self.grid)):
+            try:
+                self.remove_row(i)
+                cleared += 1
+            except ValueError:
+                pass
+        return cleared
+    
+    def scoring(self, line):
+        switcher = {
+            1: 40,
+            2: 100,
+            3: 300,
+            4: 1200
+        }
+        return switcher.get(line, 0)
 
 class Tetris(Configuration, Shapes):
     def __init__(self, width, height, infinite, type_list):
-        # super(Tetris, self).__init__(infinite, type_list)
+        # TODO: fugure out what 
         super(Tetris, self).__init__(width, height)
+        Shapes.__init__(self, infinite, type_list)
         self.turn = 0
         self.score = 0
 
     def run(self):
         self.turn += 1
         shape = self.generate(self.turn)
-        print self.grid
         print shape
-        x = input("input x coordinate")
+        print self
+        x = input("input x coordinate: ")
         self.fall(shape, x)
-        for i in range(len(self.grid)):
-            try:
-                self.remove_row(i + 1)
-            except ValueError:
-                pass
+        print self
+        self.score += self.scoring(self.clear())
+        print self.score
+        
