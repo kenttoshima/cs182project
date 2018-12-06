e40074334a8e6fc91f2881a5470def8936c4cf9a
kenttoshima <24969845+kenttoshima@users.noreply.github.com>
2018-12-06 11:43:37 -0500

    added epsilon-greedy feature

--- agent.py
+++ agent.py
@@ -1,42 +1,49 @@
 from game import Tetris, Configuration, Action, State, InvalidMoveError, GameOverError
+from random import random, choice
 
 class Agent(object):
-    def __init__(self, width = 10, height = 20, delay = 20, alpha = 0.6):
+    def __init__(self, width = 10, height = 20, delay = 20, alpha = 0.6, epsilon = 0.8):
         self.width = width
         self.height = height
         self.delay = delay
         self.alpha = alpha
+        self.epsilon = epsilon
         self.qvalues = {}
 
     def learn(self):
+        alpha = self.alpha
+        epsilon = self.epsilon
         tetris = Tetris(self.width, self.height, infinite=True, type_list=[])
         while True:
             tetris.next_turn()
             successor_list = self.getSuccessor(tetris)
             maxQvalue = float("-inf")
             bestStateAndAction = None
-            for stateAndAction in successor_list:
-                # print stateAndAction[0], stateAndAction[1]
-                if self.query(stateAndAction) > maxQvalue:
-                    bestStateAndAction = stateAndAction
-                    maxQvalue = self.query(stateAndAction)
-            # if all the successors raise GameOverError
-            if bestStateAndAction == None:
+            if not successor_list:
                 nextAction = Action(1, 0)
+            elif random() > (1 - epsilon):
+                (state, nextAction) = choice(successor_list)
+                self.query((state, nextAction))
             else:
+                for stateAndAction in successor_list:
+                    # print stateAndAction[0], stateAndAction[1]
+                    if self.query(stateAndAction) > maxQvalue:
+                        bestStateAndAction = stateAndAction
+                        maxQvalue = self.query(stateAndAction)
                 _state, nextAction = bestStateAndAction
             try:
-                self.alpha *= 0.99
+                alpha *= 0.99
+                epsilon *= 0.8
                 tetris.drop(nextAction)
                 prev_turn = tetris.turn - self.delay
                 if prev_turn > 0:
                     reward = tetris.score - tetris.history[prev_turn][1]
-                    self.qvalueUpdate(tetris.history[prev_turn][0], reward)
+                    self.qvalueUpdate(tetris.history[prev_turn][0], reward, alpha)
             except GameOverError:
                 prev_turn = tetris.turn - self.delay
                 if prev_turn > 0:
                     reward = tetris.score - tetris.history[prev_turn][1] - 1000
-                    self.qvalueUpdate(tetris.history[prev_turn][0], reward)
+                    self.qvalueUpdate(tetris.history[prev_turn][0], reward, alpha)
                 print tetris
                 print "Game Over with score " + str(tetris.score)
                 break
@@ -50,8 +57,8 @@ class Agent(object):
         # print self.qvalues[key]
         return self.qvalues[key]
 
-    def qvalueUpdate(self, key, updateValue):
-        self.qvalues[key] = (1 - self.alpha) * self.query(key) + self.alpha * updateValue
+    def qvalueUpdate(self, key, updateValue, alpha):
+        self.qvalues[key] = (1 - alpha) * self.query(key) + alpha * updateValue
 
     def getSuccessor(self, tetris):
         nextShape = tetris.shape_list[tetris.turn]
