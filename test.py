e17fd99c82b0b28e456fbd5fd2a5c0b6d3fa4ead
kenttoshima <24969845+kenttoshima@users.noreply.github.com>
2018-12-06 10:52:35 -0500

    slight change in test.py

--- test.py
+++ test.py
@@ -2,3 +2,8 @@ if __name__ == '__main__':
     from agent import Agent
     agent = Agent()
     agent.learn()
+    print agent.qvalues
+    agent.learn()
+    print agent.qvalues
+    agent.learn()
+    print agent.qvalues
